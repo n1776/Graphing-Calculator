d58acdc9bb60015d7a304a8972580493c76eff4f
Niklas Sheth <niklassheth@gmail.com>
2020-01-09 08:11:20 -0500

    add division tests

--- test/org/hypergolic/calculator/parser/ParserTest.java
+++ test/org/hypergolic/calculator/parser/ParserTest.java
@@ -6,14 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest
 {
+
+    public static final double DELTA = 0.0001;
+
     @Test
     void parseToAST()
     {
-        assertEquals(2, Parser.parseToAST("1+1").evaluate(), 0.001);
-        assertEquals(7, Parser.parseToAST("1+2*3").evaluate(), 0.001);
-        assertEquals(9, Parser.parseToAST("(1+2)*3").evaluate(), 0.001);
-        assertEquals(4, Parser.parseToAST("9+2-7").evaluate(), 0.001);
-        assertEquals(1, Parser.parseToAST("2*3-5").evaluate(), 0.001);
+        assertEquals(2, Parser.parseToAST("1+1").evaluate(), DELTA);
+        assertEquals(7, Parser.parseToAST("1+2*3").evaluate(), DELTA);
+        assertEquals(9, Parser.parseToAST("(1+2)*3").evaluate(), DELTA);
+        assertEquals(4, Parser.parseToAST("9+2-7").evaluate(), DELTA);
+        assertEquals(1, Parser.parseToAST("2*3-5").evaluate(), DELTA);
+        assertEquals(5, Parser.parseToAST("10/5+3").evaluate(), DELTA);
+        assertEquals(9, Parser.parseToAST("6/2*(1+2)").evaluate(), DELTA);
 
         assertThrows(MismatchedParenthesisException.class,
                 () -> Parser.parseToAST("(1+1"));
