e3f7884b86d3f6f61e1445ab8712c10660c0045b
Niklas Sheth <niklassheth@gmail.com>
2020-01-09 08:48:09 -0500

    create ImplicitMultiplicationVisitor

---
+++ src/org/hypergolic/calculator/parser/ImplicitMultiplicationVisitor.java
@@ -0,0 +1,68 @@
+package org.hypergolic.calculator.parser;
+
+import java.util.ArrayList;
+
+public class ImplicitMultiplicationVisitor implements Visitor
+{
+
+    private ArrayList<Token> tokens = new ArrayList<>();
+    private boolean shouldEmitMultiply = false;
+
+    public ArrayList<Token> getResult()
+    {
+        return tokens;
+    }
+
+    private void emitMultiply() {tokens.add(new MultiplicationOperator());}
+
+    @Override
+    public void visit(LeftParenthesis leftParenthesis)
+    {
+        if (shouldEmitMultiply)
+            emitMultiply();
+        shouldEmitMultiply = false;
+        tokens.add(leftParenthesis);
+    }
+
+    @Override
+    public void visit(RightParenthesis rightParenthesis)
+    {
+        shouldEmitMultiply = false;
+        tokens.add(rightParenthesis);
+    }
+
+    @Override
+    public void visit(Constant constant)
+    {
+        shouldEmitMultiply = true;
+        tokens.add(constant);
+    }
+
+    @Override
+    public void visit(AdditionOperator operator)
+    {
+        shouldEmitMultiply = false;
+        tokens.add(operator);
+    }
+
+    @Override
+    public void visit(SubtractionOperator operator)
+    {
+        shouldEmitMultiply = false;
+        tokens.add(operator);
+    }
+
+    @Override
+    public void visit(MultiplicationOperator operator)
+    {
+        shouldEmitMultiply = false;
+        tokens.add(operator);
+    }
+
+    @Override
+    public void visit(DivisionOperator operator)
+    {
+        shouldEmitMultiply = false;
+        tokens.add(operator);
+    }
+}
