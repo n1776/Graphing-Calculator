c3e4df8c7bd22bd963a97995c593e7757923fe1f
Niklas Sheth <niklassheth@gmail.com>
2020-01-11 13:33:54 -0500

    add tests for equals() and hashCode()

--- test/org/hypergolic/calculator/parser/ParserTest.java
+++ test/org/hypergolic/calculator/parser/ParserTest.java
@@ -13,6 +13,14 @@ class ParserTest
         assertEquals(expectedValue, Parser.parseToAST(expr).evaluate(0), DELTA);
     }
 
+    private void testExpr(String expr, String equalExpr)
+    {
+        //test equals() implementation
+        assertEquals(Parser.parseToAST(expr), Parser.parseToAST(equalExpr));
+        //test hashCode() implementation
+        assertEquals(Parser.parseToAST(expr).hashCode(), Parser.parseToAST(equalExpr).hashCode());
+    }
+
     @Test
     void parseToAST()
     {
@@ -24,6 +32,10 @@ class ParserTest
         testExpr("10/5+3", 5);
         testExpr("6/2(1+2)", 9);
         testExpr("(2)(3)(2)", 12);
+
+        testExpr("1", "(1)");
+        testExpr("2x", "2*x");
+        testExpr("2x^2+9x-1", "2*(x^2)+9*x-1");
 
         assertThrows(MismatchedParenthesisException.class,
                 () -> Parser.parseToAST("(1+1"));
