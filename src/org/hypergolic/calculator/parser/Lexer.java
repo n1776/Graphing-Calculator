f8bc35bd8fa58707aa5b72cf4a1cfdd011ccae75
Niklas Sheth <niklassheth@gmail.com>
2020-01-15 12:32:11 -0500

    remove Lexer.LexerState.FUNCTION

--- src/org/hypergolic/calculator/parser/Lexer.java
+++ src/org/hypergolic/calculator/parser/Lexer.java
@@ -24,7 +24,6 @@ public class Lexer
     {
         //starting state of lexer
         INITIAL,
-        FUNCTION,
         //a number is being processed
         NUMBER,
         //a single character operator is being processed
