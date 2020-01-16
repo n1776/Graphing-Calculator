bd30e820b028f1a17e5116ac6c35ae937ca98f53
Niklas Sheth <niklassheth@gmail.com>
2020-01-15 19:05:46 -0500

    add timing code to CommandLine

--- src/org/hypergolic/calculator/CommandLine.java
+++ src/org/hypergolic/calculator/CommandLine.java
@@ -3,10 +3,9 @@ package org.hypergolic.calculator;
 import org.hypergolic.calculator.parser.*;
 
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintStream;
+import java.text.DecimalFormat;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -15,6 +14,10 @@ public class CommandLine
     private final InputStream inputStream;
     private final PrintStream printStream;
     private final Scanner scanner;
+
+    private boolean enableTiming = true;
+    private long previousTime;
+    DecimalFormat timeFormat;
 
     public CommandLine()
     {
@@ -26,6 +29,9 @@ public class CommandLine
         this.inputStream = inputStream;
         this.printStream = printStream;
         scanner = new Scanner(inputStream);
+
+        //always add leading 0, display 4 digits precision with trailing 0s
+        timeFormat = new DecimalFormat("0.0000");
     }
 
     private boolean shouldContinue()
@@ -42,12 +48,21 @@ public class CommandLine
         {
             printStream.print("> ");
             String input = scanner.nextLine();
+            if (enableTiming) {
+                previousTime = System.nanoTime();
+            }
             Lexer lexer = new Lexer(constants);
             Evaluable ast = Parser.parseToAST(lexer.lexExpression(input));
             if (ast.hasVariable())
                 printStream.println("[anonymous function]");
             else
                 printStream.println(ast.evaluate(Double.NaN));
+
+            if (enableTiming) {
+                long currentTime = System.nanoTime();
+                double elapsedTimeInMs = (currentTime - previousTime) / 1e6;
+                System.out.println("Time elapsed: " + timeFormat.format(elapsedTimeInMs) + "ms");
+            }
 
         }
     }
