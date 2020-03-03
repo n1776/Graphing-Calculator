951e4d3cde92e4069d893c4dbcd5fa8442423ed8
Niklas Sheth <niklassheth@gmail.com>
2020-03-02 19:51:10 -0500

    add Function token, not implemented yet

---
+++ src/org/hypergolic/calculator/parser/Function.java
@@ -0,0 +1,11 @@
+package org.hypergolic.calculator.parser;
+
+public class Function implements Token
+{
+    @Override
+    public void accept(Visitor visitor)
+    {
+        //visitor.visit(this);
+    }
+
+}
